900ec0463628c8c1b405033c2e16f04b5dbb5393
remybms <112947392+remybms@users.noreply.github.com>
2025-05-21 11:15:11 +0200

    add login options

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,21 +1,57 @@
+"use client"
+
+import React from 'react';
 import styles from '../../CSS/login.module.css';
 import Link from 'next/link';
+import { saveTokenToLocalStorage } from '@/jwt';
+import { useRouter } from "next/navigation";
+
 
 export default function Login() {
+
+    const [mail, setMail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const router = useRouter()
+    
+
+    async function login(e) {
+        e.preventDefault();
+        var data
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "email": mail,
+                    "password": password
+                }),
+            });
+
+            data = await response.json();
+            saveTokenToLocalStorage(data.auth_token);
+            router.push('/')
+        } catch (error) {
+            console.error('Erreur lors de l\'envoi des données à l\'API :', error);
+        }
+    }
+
+
     return (
         <main className={styles.login}>
             <div className={styles.text}>
                 <h1>
                     Welcome back !
                 </h1>
-                <form className={styles.form}>
-                    <input type="email" placeholder="Email" className={styles.input}/>
-                    <input type="password" placeholder="Mot de passe" className={styles.input}/>
+                <form className={styles.form} onSubmit={login}>
+                    <input type="email" placeholder="Email" className={styles.input} onChange={(e) => setMail(e.target.value)} required />
+                    <input type="password" placeholder="Mot de passe" className={styles.input} onChange={(e) => setPassword(e.target.value)} required />
                     <button type="submit" className={styles.button}>Se connecter</button>
                 </form>
                 <Link href='/auth/signin'>Pas encore de compte ? S'inscrire</Link>
             </div>
-            <img src="/images/logo_noledge.png" alt="Login" className={styles.logo}/>
+            <img src="/images/logo_noledge.png" alt="Login" className={styles.logo} />
         </main>
     )
 }
