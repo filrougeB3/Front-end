1c8fb9bd137d907063a473be4443a50bdb28b5e6
remybms <112947392+remybms@users.noreply.github.com>
2025-06-02 17:13:00 +0200

    get quizzes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,34 @@
+"use client"
+
 import Image from "next/image";
 import Link from 'next/link';
 import NavBar from "../components/asidemenu";
 import "../app/CSS/globals.css";
+import React from "react";
 
 export default function Home() {
-  const fakeQuizzes = [
-    { id: 1, title: "Quiz Football", author: "Admin" },
-    { id: 2, title: "Culture Générale", author: "Marie" },
-    { id: 3, title: "Histoire de France", author: "Jean" },
-  ];
+
+  const [quizzes, setQuizzes] = React.useState([]);
+
+  async function getQuiz() {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/quiz`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const data = await response.json();
+      setQuizzes(data)
+    }
+    catch (error) {
+      console.error('Erreur lors de la récupération des données de l\'API :', error);
+    }
+  }
+
+  React.useEffect(() => {
+    getQuiz()
+  }, [])
 
   const fakeCategories = [
     "Sport",
@@ -30,9 +50,9 @@ export default function Home() {
           />
         </div>
         <div className="popular-quiz">
-          <h2>Quiz populaires</h2>       
+          <h2>Quiz populaires</h2>
           <div className="quiz-list">
-            {fakeQuizzes.map((quiz) => (
+            {quizzes.map((quiz) => (
               <div key={quiz.id} className="quiz-item">
                 <Link href={`/quiz/${quiz.id}`}>
                   <h3>{quiz.title}</h3>
@@ -50,9 +70,9 @@ export default function Home() {
                 </Link>
               </div>
             ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
